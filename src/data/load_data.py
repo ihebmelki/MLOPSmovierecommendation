8867dcb299b02d83d48f93fa1c3d7d4edb846bc4
Ranim Ben Mrad <mradranom45@gmail.com>
2025-12-07 16:43:03 +0100

    Modifie load_ratings pour lire ratings_processed.csv

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -2,9 +2,9 @@ import pandas as pd
 from pathlib import Path
 
 def load_ratings():
-    path = Path("data/raw/ratings.csv")
+    path = Path("data/processed/ratings_processed.csv")
     return pd.read_csv(path)
 
 def load_movies():
-    path = Path("data/raw/movies.csv")
+    path = Path("data/processed/movies_processed.csv")
     return pd.read_csv(path)
